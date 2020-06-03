991b2ceee7a625e916eb9a7bb7afbc8a0c2be4d4
Dylan Henderson <dylan.henderson@outlook.com>
2020-06-03 10:06:51 +1000

    Changed logger

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(logger('dev'));
+app.use(logger('common'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
